11c7206a379165dd8ddd8113f87cb00f0ab700cc
Sam Jake <samjulian88@gmail.com>
2017-12-14 00:39:39 +0530

    Second Plot

--- plot1.r
+++ plot1.r
@@ -5,7 +5,7 @@
 
 data_split <- split(data,data$year)
 sum_emissions <- lapply(data_split,function(x){sum(x$Emissions)})
-setwd("J:/R/course2")
+setwd("J:/R/expdata_proj")
 png(filename = "./Images/plot1.png")
 par(mfrow=c(1,1),mar=c(5,5,5,5))
 plot(as.integer(names(sum_emissions)),sum_emissions,main="US PM25 Emissions by year",xlab = "Year",ylab = "Emission in tons",pch=19,col=c(1,2,3,4))
---
+++ plot2.r
@@ -0,0 +1,8 @@
+data_balt <- subset(data,data$fips=="24510")
+data_balt_split <- split(data_balt,data_balt$year)
+sum_balt_emissions <- lapply(data_balt_split,function(x){sum(x$Emissions)})
+setwd("J:/R/expdata_proj")
+png(filename = "./Images/plot2.png")
+par(mfrow=c(1,1),mar=c(5,5,5,5))
+plot(as.integer(names(sum_balt_emissions)),sum_balt_emissions,main="Baltimore City PM25 Emissions by year",xlab = "Year",ylab = "Emission in tons",pch=19,col="red")
+dev.off()
