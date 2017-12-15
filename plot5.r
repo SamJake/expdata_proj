b542c20c909dbe81f5a8fbb5eea1dccd2f95a7e0
Sam Jacob Julian <sajulian@in.ibm.com>
2017-12-16 03:14:36 +0530

    Fifth plot

---
+++ plot5.r
@@ -0,0 +1,19 @@
+
+#subsetting only Vehicle EI.Sectors...
+src_class <- src_class[,c(1,4)]
+uniq_sec <- as.character(unique(src_class$EI.Sector))
+index3 <- grep("Vehicle",uniq_sec)
+veh_sec <- uniq_sec[index3]
+veh_sec_class <- subset(src_class,EI.Sector %in% veh_sec)
+
+#Summarizing Emissions data for Baltimore city...
+veh_data <- merge(data_balt,veh_sec_class,by="SCC")
+veh_grouped_data <- group_by(veh_data,EI.Sector,year) %>% summarise(sum(Emissions))
+names(veh_grouped_data)[3] <- "Emissions"
+
+#Plotting...
+setwd("C:/Users/SamJacobJulian/Desktop/Rstd/expdata_proj")
+png(filename = "./Images/plot5.png")     
+ggplot(veh_grouped_data,aes(year,Emissions,color=EI.Sector))+geom_point(size=2)+geom_line()+
+  ggtitle("Vehicle Emissions in Baltimore over the years")
+dev.off()
